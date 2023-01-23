5134edb1bfb35db7135d0e948b0f47682b33f565
Grimhunter <62704709+Grimhunter9@users.noreply.github.com>
2023-01-23 21:56:05 +0100

    fix aos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import '../styles/globals.css'
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'
 import Head from "../Html/html";
 import type { AppProps } from 'next/app'
 import Foot from "../Components/Footer/footer";
 
 export default function App({ Component, pageProps }: AppProps) {
+  
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: true })
+  }, [])
+
   return (
     <>
       <Head />
