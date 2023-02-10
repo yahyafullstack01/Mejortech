63522bbea24ba183f812afd0f0684e1190e7c8cc
yahya alloucha <yahyafullstack01@gmail.com>
2023-02-10 23:01:42 +0100

    with Links

--- Components/Join_us/join.tsx
+++ Components/Join_us/join.tsx
@@ -26,7 +26,7 @@ export default function Join() {
                     <h1 className={styles.title}>EDUCATION</h1>
                     <span className={styles.time}>10.21 7:00-8:00PM</span>
                     <p className={styles.info}>It All Begins With An Idea. Maybe You Want To Launch A Business. Maybe You Want To Turn A Hobby Into Something More.</p>
-                    <Link className={styles.link} passHref href=""><button className={styles.btn}>Click here</button></Link>
+                    <Link className={styles.link} passHref href="/Education_Page/Education"><button className={styles.btn}>Click here</button></Link>
                 </div>
                 <div data-aos="zoom-in"  data-aos-duration="2000" className={styles.card}>
                     <img
@@ -37,7 +37,7 @@ export default function Join() {
                     <h1 className={styles.title}>MARKETING</h1>
                     <span className={styles.time}>10.21 7:00-8:00PM</span>
                     <p className={styles.info}>It All Begins With An Idea. Maybe You Want To Launch A Business. Maybe You Want To Turn A Hobby Into Something More.</p>
-                    <Link className={styles.link} passHref href=""><button className={styles.btn}>Click here</button></Link>
+                    <Link className={styles.link} passHref href="/Marketing_Page/Marketing"><button className={styles.btn}>Click here</button></Link>
                 </div>
             </div>
         </div>
