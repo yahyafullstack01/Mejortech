7654e68c1607bf0f6dd428ce711364a84f959ffc
yahya alloucha <yahyafullstack01@gmail.com>
2023-02-11 00:55:00 +0100

    with change function

--- Components/What_we_do/we_do.tsx
+++ Components/What_we_do/we_do.tsx
@@ -1,6 +1,35 @@
+import { useState } from "react";
 import styles from "../What_we_do/we_do.module.css";
 
 export default function OurWork() {
+    const [changediv, setChangediv] = useState(false);
+
+    function LearnEducation (){
+        if(!changediv){
+            return(
+            
+                <img
+
+                src={"/library.jpg"}
+                alt="The education picture"
+                className={`${styles.pic} ${changediv? `${styles.animating}`: ""}`}
+                />
+            )
+        }
+        else {
+            return (
+                <img
+
+                src={"/marketing.jpg"}
+                alt="The education picture"
+                className={`${styles.pic} ${changediv? `${styles.animating}`: ""}`}
+                />
+           )
+        }
+
+    }
+    
+
     return (
         <div data-aos="fade-in"  data-aos-duration="2000" id="Our_work" className={styles.container}>
             <h1 className={`${styles.header} ${"text-center"}`}>WHAT WE DO</h1>
@@ -13,21 +42,20 @@ export default function OurWork() {
                     />
                     <div className={styles.text_block}>
                         <h1 className={`${styles.header} ${"text-center"}`}>IT PROJECTS</h1>
-                        <p className={styles.text}>Find Out About Our Projects, Methods, And The Results Of Our Decades Of Advocacy.</p>
-                        <button className={styles.btn}>Learn more</button>
+                        <p className={styles.text}>Find Out About Our Projects, Methods, And how we work.</p>
+                        <button className={styles.btn}  >Learn more</button>
                     </div>
                 </div>
                 <div data-aos="fade-left"  data-aos-duration="2000" className={styles.block} >
                     <div className={styles.text_block}>
                         <h1 className={`${styles.header} ${"text-center"}`}>EDUCATION</h1>
                         <p className={styles.text}>Ready To Take The Next Step? You Can Learn New Things And Improve Yourself In The Future.</p>
-                        <button className={styles.btn}>Learn more</button>
+                        <button className={styles.btn} onClick={()=> setChangediv(!changediv)}>{changediv? "Go Back" : "Learn more"}</button>
                     </div>
-                    <img
-                        src="/library.jpg"
-                        alt="The education picture"
-                        className={`${styles.pic}`}
-                    />
+                    
+                        {LearnEducation()}
+                    
+
                 </div>
                 <div data-aos="fade-right"  data-aos-duration="2000" className={styles.block}>
                     <img
