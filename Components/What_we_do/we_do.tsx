8b0ca02c225fa05c03c570396ee82364457b78e2
Grimhunter <62704709+Grimhunter9@users.noreply.github.com>
2023-02-11 13:36:21 +0100

    Added Text on pic change

--- Components/What_we_do/we_do.tsx
+++ Components/What_we_do/we_do.tsx
@@ -21,7 +21,17 @@ export default function OurWork() {
         }
         else {
             return (
-                <h1>Hello there</h1>
+                <div className={styles.Text_block_Learn}>
+                    <h1 className={styles.title_Learn}>Our Work consists Of :</h1>
+                    <ul className={styles.list_con_Learn}>
+                        <li>Creating Web and Mobile Application.</li>
+                        <li>Fetching Api's from different servers.</li>
+                        <li>Storing Data in many types of softwares Like <code style={{ color: "#907730" }}>(Airtable, MongoDb, MySql)</code>.</li>
+                        <li>Working with a proprietary issue tracking product such as <code style={{ color: "#907730" }}>(Jira)</code>to manage projects when working in a team.</li>
+                        <li>Using Desgin tools such as <code style={{ color: "#907730" }}>(figma)</code> to Create or Implement App Desgins.</li>
+                        <li>The use of Headless CMS such as <code style={{ color: "#907730" }}>(Sanity.io)</code> that helps the client to add, change or delete content in the App</li>
+                    </ul>
+                </div>
             )
         }
 
@@ -39,11 +49,17 @@ export default function OurWork() {
         }
         else {
             return (
-                <img
-                    src={"/marketing.jpg"}
-                    alt="The education picture"
-                    className={`${styles.pic}`}
-                />
+                <div className={styles.Text_block_Learn}>
+                    <h1 className={styles.title_Learn}>You will Learn:</h1>
+                    <ul className={styles.list_con_Learn}>
+                        <li>Frontend development teaching you the basics  <code style={{ color: "#907730" }}>(Html, Css, Javascript)</code>.</li>
+                        <li>From Beginner to Expert in English</li>
+                        <li>Become a Fullstack developer , Learn how to create professonal Applications Using different frameworks such as <code style={{ color: "#907730" }}>(React js, Next js, Node js)</code>.</li>
+                        <li></li>
+                        <li></li>
+
+                    </ul>
+                </div>
             )
         }
 
@@ -74,11 +90,6 @@ export default function OurWork() {
 
                 {/*IT Block*/}
                 <div data-aos="fade-right" data-aos-duration="2000" className={styles.block}>
-                    {/* <img
-                        src="/Laptop.jpg"
-                        alt="The IT picture"
-                        className={`${styles.pic}`}
-                    /> */}
                     <div className={styles.con}>
                         {LearnIT()}
                     </div>
@@ -96,37 +107,23 @@ export default function OurWork() {
                         <p className={styles.text}>Ready To Take The Next Step? You Can Learn New Things And Improve Yourself In The Future.</p>
                         <button className={styles.btn} onClick={() => setChangediv2(!changediv2)}>{changediv2 ? "Go Back" : "Learn more"}</button>
                     </div>
-
                     <div className={`${styles.con}`}>
                         {LearnEducation()}
                     </div>
-
-                    {/* <img
-                        src="/library.jpg"
-                        alt="The marketing picture"
-                        className={`${styles.pic}`}
-                    /> */}
-
                 </div>
 
                 {/*Marketing Block*/}
                 <div data-aos="fade-right" data-aos-duration="2000" className={styles.block}>
-                    {/* <img
-                        src="/marketing.jpg"
-                        alt="The marketing picture"
-                        className={`${styles.pic}`}
-                    /> */}
-
                     <div className={styles.con}>
                         {LearnMarketing()}
                     </div>
-
                     <div className={styles.text_block}>
                         <h1 className={`${styles.header} ${"text-center"}`}>MARKETING</h1>
                         <p className={styles.text}>Enter  A World That Adapts This New Generation, That Opens Many Doors To Success.</p>
                         <button className={styles.btn} onClick={() => setChangediv3(!changediv3)}>{changediv3 ? "Go Back" : "Learn more"}</button>
                     </div>
                 </div>
+
             </div>
         </div>
     )
