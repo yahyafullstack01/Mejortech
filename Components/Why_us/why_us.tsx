972ea99f31e4eeda3fa125e766dd447d0dabf740
Grimhunter <62704709+Grimhunter9@users.noreply.github.com>
2023-02-17 21:15:30 +0100

    fix

--- Components/Why_us/why_us.tsx
+++ Components/Why_us/why_us.tsx
@@ -7,7 +7,7 @@ export default function Why_us() {
             <h1 data-aos="fade-right" className={styles.header}>Why choose Us</h1>
 
             <div className={styles.grid}>
-                <ul className={styles.p}>
+                <ul data-aos="fade-right" className={styles.p}>
                     <li>We are highly qualified engineers that possess superior technical expertise and practical understanding of the most recent software standards.</li>
                     <br></br>
                     <li>having amassed a sizable body of expertise, which we use to provide solutions that satisfy the demands, expectations, and financial constraints of our clients.</li>
@@ -15,6 +15,7 @@ export default function Why_us() {
                     <li> Our multilingual staff that speaks English, Arabic, Russian, Spanish, French and Having many years of experience working with clients from around the world.</li>
                 </ul>
                 <img
+                    data-aos="fade-left"
                     src="/why_us.jpg"
                     alt="The education picture"
                     className={`${styles.pic}`}
