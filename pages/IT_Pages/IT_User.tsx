a5be8020cec39931ad2c5ca9c181d85b79414f02
Grimhunter <62704709+Grimhunter9@users.noreply.github.com>
2023-02-27 16:14:33 +0100

    It_user fixed

--- pages/IT_Pages/IT_User.tsx
+++ pages/IT_Pages/IT_User.tsx
@@ -1,13 +1,55 @@
 import Navbar from "../../Components/IT-comp/Navbar_IT/navbar_it";
 import Intro from "../../Components/IT-comp/Introduction_IT/intro_it";
-import { useContext } from "react";
 import { AppContext } from "../../Mycontext/context";
+import { useTranslation } from "next-i18next";
+import { useState } from "react";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { GetStaticProps } from "next";
+import SectionContact from "../../Components/IT-comp/Form/SectionContact";
+import SignUpSection from "../../Components/IT-comp/Form/SignUpSection";
+import LogInSection from "../../Components/IT-comp/Form/LogInSection";
+
 
 export default function IT_User() {
+
+    const { t, i18n } = useTranslation("ITPage");
+    const isArabic = i18n.language === "ar";
+
+    //The arr information of the Navbar_it section
+    const Signing: any = t('Signing', { returnObjects: true });
+
+    //The arr information of the Intro_it section
+    const Intro_info: any = t('Intro_info', { returnObjects: true });
+
+    const [FormContainer, setFormContainer] = useState(<SectionContact />);
+
+    const ContactIt = () => setFormContainer(<SectionContact />);;
+    const LogForm = () => setFormContainer(<LogInSection />);
+    const SignForm = () => setFormContainer(<SignUpSection />);
+
+
     return (
-        <>
-            <Navbar />
-            <Intro />
-        </>
+        <AppContext.Provider value={{
+            t, Signing, Intro_info,
+            isArabic,
+            SectionContact, SignUpSection, LogInSection, FormContainer,
+            LogForm, SignForm, ContactIt
+        }}>
+            <>
+                <Navbar />
+                <Intro />
+            </>
+        </AppContext.Provider>
+
     )
 } 
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+    const lang = locale ?? 'en';
+
+    return {
+        props: {
+            ...(await serverSideTranslations(lang, ['ITPage', 'Home'])),
+        },
+    };
+};
