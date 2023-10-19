a9bdbd48da77ee939cf3baec47f0ebc91a35b63a
Grimhunter <pandatech1999@gmail.com>
2023-10-20 01:30:20 +0200

    animation added

--- Components/Home-comp/Our_Services/our_services.tsx
+++ Components/Home-comp/Our_Services/our_services.tsx
@@ -13,7 +13,7 @@ export default function Team() {
     console.log(t("Our_services_type_Frontend_training"));
     return (
         <div
-            id="Our_services" className={`${styles.container} ${"flex flex-col relative"}`}>
+            id="Our_services"  className={`${styles.container} ${"flex flex-col relative"}`}>
             <video
                 autoPlay={true}
                 loop={true}
@@ -29,11 +29,12 @@ export default function Team() {
                 <img
                     src={"/Website_develop.jpg"}
                     alt="The Website developing picture"
+                    data-aos="fade-right" data-aos-duration="2000"
                     className={styles.img}
                 />
                 <h3 className={`${styles.about} ${isArabic ? styles.ar : ""}`}>{Our_services_Types_about[0]}</h3>
                 <p className={`${styles.type} ${isArabic ? styles.ar : ""}`}>{Our_services_service[0]}</p>
-                <ol className={styles.ol}>
+                <ol className={styles.ol} data-aos="fade-in" data-aos-duration="2000">
                     <li className={`${styles.list} ${isArabic ? styles.ar : ""}`}><strong className={`${styles.types} ${isArabic ? styles.ar : ""}`}>{Our_services_type_website_Dev[0]}</strong> {Our_services_type_website_Dev_info[0]}</li>
                     <li className={`${styles.list} ${isArabic ? styles.ar : ""}`}><strong className={`${styles.types} ${isArabic ? styles.ar : ""}`}>{Our_services_type_website_Dev[1]}</strong> {Our_services_type_website_Dev_info[1]}</li>
                     <li className={`${styles.list} ${isArabic ? styles.ar : ""}`}><strong className={`${styles.types} ${isArabic ? styles.ar : ""}`}>{Our_services_type_website_Dev[2]}</strong> {Our_services_type_website_Dev_info[2]}</li>
@@ -49,11 +50,12 @@ export default function Team() {
                 <img
                     src={"/Mobile_apps.jpg"}
                     alt="The Mobile picture"
+                    data-aos="fade-left" data-aos-duration="2000"
                     className={styles.img}
                 />
                 <h3 className={`${styles.about} ${isArabic ? styles.ar : ""}`}>{Our_services_Types_about[1]}</h3>
                 <p className={`${styles.type} ${isArabic ? styles.ar : ""}`}>{Our_services_service[1]}</p>
-                <ol className={styles.ol}>
+                <ol className={styles.ol} data-aos="fade-in" data-aos-duration="2000">
                     <li className={`${styles.list} ${isArabic ? styles.ar : ""}`}><strong className={`${styles.types} ${isArabic ? styles.ar : ""}`}>{Our_services_type_Mobile_Dev[0]}</strong> {Our_services_type_Mobile_Dev_info[0]}</li>
                     <li className={`${styles.list} ${isArabic ? styles.ar : ""}`}><strong className={`${styles.types} ${isArabic ? styles.ar : ""}`}>{Our_services_type_Mobile_Dev[1]}</strong> {Our_services_type_Mobile_Dev_info[1]}</li>
                     <li className={`${styles.list} ${isArabic ? styles.ar : ""}`}><strong className={`${styles.types} ${isArabic ? styles.ar : ""}`}>{Our_services_type_Mobile_Dev[2]}</strong> {Our_services_type_Mobile_Dev_info[2]}</li>
@@ -67,11 +69,12 @@ export default function Team() {
                 <img
                     src={"/Frontend_training.jpg"}
                     alt="The Frontend training picture"
+                    data-aos="fade-right" data-aos-duration="2000"
                     className={styles.img}
                 />
                 <h3 className={`${styles.about} ${isArabic ? styles.ar : ""}`}>{Our_services_Types_about[2]}</h3>
                 <p className={`${styles.type} ${isArabic ? styles.ar : ""}`}>{Our_services_service[2]}</p>
-                <ol className={styles.ol}>
+                <ol className={styles.ol} data-aos="fade-in" data-aos-duration="2000">
                     <li className={`${styles.list} ${isArabic ? styles.ar : ""}`}><strong className={`${styles.types} ${isArabic ? styles.ar : ""}`}>{Our_services_type_Frontend_training[0]}</strong> {Our_services_type_Frontend_training_info[0]}</li>
                     <li className={`${styles.list} ${isArabic ? styles.ar : ""}`}><strong className={`${styles.types} ${isArabic ? styles.ar : ""}`}>{Our_services_type_Frontend_training[1]}</strong> {Our_services_type_Frontend_training_info[1]}</li>
                     <li className={`${styles.list} ${isArabic ? styles.ar : ""}`}><strong className={`${styles.types} ${isArabic ? styles.ar : ""}`}>{Our_services_type_Frontend_training[2]}</strong> {Our_services_type_Frontend_training_info[2]}</li>
@@ -85,11 +88,12 @@ export default function Team() {
                 <img
                     src={"/Digital_marketing.jpg"}
                     alt="The Frontend training picture"
+                    data-aos="fade-left" data-aos-duration="2000"
                     className={styles.img}
                 />
                 <h3 className={`${styles.about} ${isArabic ? styles.ar : ""}`}>{Our_services_Types_about[3]}</h3>
                 <p className={`${styles.type} ${isArabic ? styles.ar : ""}`}>{Our_services_service[3]}</p>
-                <ol className={styles.ol}>
+                <ol className={styles.ol} data-aos="fade-in" data-aos-duration="2000">
                     <li className={`${styles.list} ${isArabic ? styles.ar : ""}`}><strong className={`${styles.types} ${isArabic ? styles.ar : ""}`}>{Our_services_type_Digital_marketing[0]}</strong> {Our_services_type_Digital_marketing_info[0]}</li>
                     <li className={`${styles.list} ${isArabic ? styles.ar : ""}`}><strong className={`${styles.types} ${isArabic ? styles.ar : ""}`}>{Our_services_type_Digital_marketing[1]}</strong> {Our_services_type_Digital_marketing_info[1]}</li>
                     <li className={`${styles.list} ${isArabic ? styles.ar : ""}`}><strong className={`${styles.types} ${isArabic ? styles.ar : ""}`}>{Our_services_type_Digital_marketing[2]}</strong> {Our_services_type_Digital_marketing_info[2]}</li>
