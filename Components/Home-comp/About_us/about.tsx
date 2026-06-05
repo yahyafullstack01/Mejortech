de7a5e1a935b523c8eb70fbded736b87b86804de
yahyafullstack01 <yahyafullstack01@gmail.com>
2026-06-05 13:46:56 +0200

    Update about.tsx

--- Components/Home-comp/About_us/about.tsx
+++ Components/Home-comp/About_us/about.tsx
@@ -24,7 +24,7 @@ export default function About() {
                     <br></br>
                     {t("Para_5")}
                     <br></br>
-                    {t("Para_6")}
+                    {t("Para_6")} Hello
                 </p>
             </div>
             <Image src="/about.jpg" alt="About" width={500}
